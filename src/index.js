0ecf1b141e4f6f557fb87383c045bd2fdb5bd848
felipedev1996 <felipepino1996@gmail.com>
2023-03-07 23:45:30 -0300

    netlify deploy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 
 
 
+import  {PORT}  from "./config";
 import app from "./app";
 import  "./database";
-import  {PORT}  from "./config";
 
 app.listen(PORT);
     console.log(`Server on port ${PORT}`);
