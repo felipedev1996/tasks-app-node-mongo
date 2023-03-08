b8f57c18930e727ba63a7cbc28678a974f331bad
felipedev1996 <felipepino1996@gmail.com>
2023-03-08 18:00:35 -0300

    vercel file add

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@
 import  { config } from "dotenv";
 config();
 
-export const MONGODB_URI =  process.env.MONGODB_URI || 'mongodb://localhost/test';
+export const MONGODB_URI =  process.env.MONGODB_URI || 'mongodb://127.0.0.1/node-task-app';
 export const PORT =  process.env.PORT || 3000;
